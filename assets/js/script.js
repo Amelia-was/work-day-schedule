c8847c37cadb56068fc9e70b531c693c3d0da7e9
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-01-10 12:37:48 -0500

    events save when edited, events saved to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,77 @@ console.log(today);
 var todaySpanEl = $("#currentDay");
 todaySpanEl.text(today);
 
+console.log(dayjs().format('H a'));
+
+// event objects
+hour9 = {
+    hr: 9,
+    event: ""
+}
+hour10 = {
+    hr: 10,
+    event: ""
+}
+hour11 = {
+    hr: 11,
+    event: ""
+}
+hour12 = {
+    hr: 12,
+    event: ""
+}
+hour13 = {
+    hr: 13,
+    event: ""
+}
+hour14 = {
+    hr: 14,
+    event: ""
+}
+hour15 = {
+    hr: 15,
+    event: ""
+}
+hour16 = {
+    hr: 16,
+    event: ""
+}
+
+// events array
+var events = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16];
+
+// save events
+// get text from each timeblock
+// save to array of objects
+// save them to local storage
+var saveEvents = function() {
+    $(".event-text").each(function(index) {
+        console.log(index + ": " + $(this).text());
+        events[index].event = $(this).text();
+    });
+
+    console.log(events);
+
+    localStorage.setItem("events", JSON.stringify(events));
+
+};
+
+// load events
+// get events from local storage 
+// update timeblocks with event text
+var loadEvents = function () {
+    events = JSON.parse(localStorage.getItem("events"));
+
+    if (!events) {
+        events = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16];
+    }
+
+    $(".event-text").each(function (index) {
+        $(this).text(events[index].event);
+        console.log(index + ": " + $(this).text());
+    });
+}
+
 // click listener for p element in time block
 $(".time-block").on("click", "p", function () {
     // get text from <p> element
@@ -46,9 +117,13 @@ $(".time-block").on("blur", "textarea", function () {
 
     // recreate p element
     var taskP = $("<p>")
-        .addClass("text-light text-left")
+        .addClass("event-text text-light text-left")
         .text(text);
 
     // replace textarea with p element
     $(this).replaceWith(taskP);
+
+    saveEvents();
 });
+
+loadEvents();
